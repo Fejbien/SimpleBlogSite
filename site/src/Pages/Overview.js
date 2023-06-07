2b666918e56ce4dead188652e1e963d475bf4386
Fabian <fabian.sucholas123@gmail.com>
2023-06-07 12:32:38 +0200

    recolation

---
+++ site/src/Pages/Overview.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+function Overview() {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost/api.php?endpoint=overview", {
+            method: "GET",
+            mode: "cors",
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                setError(error);
+                setIsLoading(false);
+            });
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        <div>
+            <h1>Data:</h1>
+            <ul>
+                {data.map((item) => (
+                    <li key={item.id}>
+                        <h1>{item.id}</h1>
+                        <h2>{item.title}</h2>
+                        <p>{item.shortText}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
