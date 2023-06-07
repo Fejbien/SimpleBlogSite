04bb899d1687ef3696f7736d886c26632b56ae6c
Fabian <fabian.sucholas123@gmail.com>
2023-06-07 12:32:23 +0200

    relocation

--- site/src/App.js
+++ site/src/App.js
@@ -1,47 +1,6 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost/api.php?endpoint=overview', {
-      method: 'GET',
-      mode: 'cors'
-    })
-      .then(response => response.json())
-      .then(data => {
-        setData(data);
-        setIsLoading(false);
-      })
-      .catch(error => {
-        setError(error);
-        setIsLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  return (
-    <div>
-      <h1>Data:</h1>
-      <ul>
-        {data.map(item => (
-          <li key={item.id}>
-            <h1>{item.id}</h1><h2>{item.title}</h2><p>{item.shortText}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+function App() {}
 
 export default App;
